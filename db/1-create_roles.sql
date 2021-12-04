cba0ed469f3e6de2fa67c44df37aa17d9c1b8194
Paweł Bucki <mrpawrob@gmail.com>
2021-12-04 17:32:03 +0100

    FIXED [db dockerfile]

--- db/1-create_roles.sql
+++ db/1-create_roles.sql
@@ -1,5 +1,6 @@
+DROP DATABASE IF EXISTS ssbd05;
 CREATE DATABASE ssbd05;
-CREATE USER 'ssbd05'@'%' IDENTIFIED BY 'yinReneif1';
+
 CREATE USER 'ssbd05admin'@'%' IDENTIFIED BY 'L0dbr0k';
 CREATE USER 'ssbd05mok'@'%' IDENTIFIED BY 'w1k1ngow1e';
 CREATE USER 'ssbd05moo'@'%' IDENTIFIED BY 'w0j0wnicy';
